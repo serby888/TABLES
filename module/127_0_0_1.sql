f04ff54ab3293ad8a8350c3c2b9b8790fa269adc
serby888 <she_ka@mail.ru>
2017-11-09 19:06:53 +0300

    com5
    + измененная БД

--- module/127_0_0_1.sql
+++ module/127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Ноя 09 2017 г., 17:26
+-- Время создания: Ноя 09 2017 г., 19:03
 -- Версия сервера: 10.1.21-MariaDB
 -- Версия PHP: 5.6.30
 
@@ -43,8 +43,7 @@ CREATE TABLE IF NOT EXISTS `uptimes` (
   `ID` int(11) NOT NULL,
   `TODAY` time NOT NULL DEFAULT '00:01:00',
   `YESTERDAY` time NOT NULL DEFAULT '00:00:00',
-  `TOTAL` time NOT NULL DEFAULT '00:01:00',
-  PRIMARY KEY (`ID`)
+  `TOTAL` time NOT NULL DEFAULT '00:01:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -57,8 +56,7 @@ CREATE TABLE IF NOT EXISTS `views` (
   `ID` int(11) NOT NULL,
   `TODAY` int(11) NOT NULL DEFAULT '1',
   `YESTERDAY` int(11) NOT NULL DEFAULT '0',
-  `TOTAL` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`ID`)
+  `TOTAL` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
